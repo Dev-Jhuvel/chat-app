16fc20b7bd43d98aa7b797cd494f273b1b46c9e8
devjhuvel <devjhuvel@gmail.com>
2025-08-21 18:57:50 +0800

    pjson

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
   console.log("PRODUCTION");
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("/:catchAll(*)", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
