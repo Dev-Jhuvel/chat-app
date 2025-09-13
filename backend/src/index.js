5f38433d50d126afb3fed0dddc0f308902cd8cf0
Dev-Jhuvel <devjhuvel@gmail.com>
2025-09-13 21:43:29 +0800

    cron route

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
-import { job } from "./lib/cron.js";
+import job from "./lib/cron.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
-  job.start;
+  job.start();
   app.use("/api/health", (req, res) => {
     res.status(200).json({ status: "ok" });
   });
